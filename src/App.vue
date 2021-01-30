ae8fe99fb99f6f7d6e89ec5d9101d503bb2da8bc
ldrovira <luisdanielrovira8@gmail.com>
2021-01-30 01:49:08 +0100

    add vite

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <section1></section1>
+  </div>
+</template>
+
+<script setup>
+import section1 from './components/section1.vue'
+</script>
+
+<style lang="sass">
+  *
+    margin: 0px
+    padding: 0px
+    list-style: none
+    text-decoration: none
+    box-sizing: border-box
+</style>
---
+++ src/components/section1.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section-1">
+    <img src="/doge.png" alt="">
+  </section>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
